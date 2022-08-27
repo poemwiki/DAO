5e9a59b6f01f70de79080558c998a051bc0716d0
Ray Victor <ray7551@gmail.com>
2022-08-28 03:24:10 +0800

    update env variables

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 // DDAO
 // mongodb+srv://<user></user>:<password>@ddao.n7b87r2.mongodb.net/?retryWrites=true&w=majority
-const host = process.env.MONGODB_HOST || 'ddao.n7b87r2.mongodb.net'
+const host = process.env.MONGODB_HOST
 const scheme = process.env.MONGODB_SCHEME || 'mongodb+srv'
 const authDB = process.env.MONGODB_DEFAULT_AUTH_DB || ''
 
