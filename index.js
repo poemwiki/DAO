2fe5d8a821a1e946b229609c4f7250e546d8262c
Ray Victor <ray7551@gmail.com>
2022-08-30 09:08:56 +0800

    format

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-require("dotenv").config()
+/* eslint-disable @typescript-eslint/no-var-requires */
+require('dotenv').config()
 
-var path = require("path")
-var express = require("express")
-var bodyParser = require("body-parser")
+var path = require('path')
+var express = require('express')
+var bodyParser = require('body-parser')
 
 var app = express()
-require("./db")
+require('./db')
 
-mintProposalModel = require("./models/proposal/mint/model")
-holderModel = require("./models/holder/model")
+const mintProposalModel = require('./models/proposal/mint/model')
+const holderModel = require('./models/holder/model')
 
 const port = process.env.PORT || 3000
 
-app.set("view engine", "ejs")
+app.set('view engine', 'ejs')
 
-app.use(express.static(path.join(__dirname, "statics")))
+app.use(express.static(path.join(__dirname, 'statics')))
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 const mode = process.argv[2]
 let serverUrl = `http://localhost:${port}`
-if (mode === "prod") serverUrl = process.env.SERVER_URL
+if (mode === 'prod') serverUrl = process.env.SERVER_URL
 
-app.get("/", function (req, res) {
-  res.render("index", {
+app.get('/', function (req, res) {
+  res.render('index', {
     daoname: process.env.DAO_NAME,
     serverUrl,
     token: process.env.TOKEN_ADDRESS,
@@ -33,85 +34,85 @@ app.get("/", function (req, res) {
   })
 })
 
-app.post("/api/holder/update", (req, res, next) => {
+app.post('/api/holder/update', (req, res, next) => {
   holderModel
     .updateHolder(req.body)
     .then(() =>
       res.json({
-        message: "success"
+        message: 'success'
       })
     )
     .catch((err) => res.status(400).json({ message: err }))
 })
 
-app.post("/api/holder/all", (req, res, next) => {
+app.post('/api/holder/all', (req, res, next) => {
   holderModel
     .getAllHolders()
     .then((holders) =>
       res.json({
-        message: "success",
+        message: 'success',
         data: holders
       })
     )
     .catch((err) => res.status(400).json({ error: err.message }))
 })
 
-app.post("/api/proposal/update", (req, res, next) => {
+app.post('/api/proposal/update', (req, res, next) => {
   var errors = []
   if (errors.length) {
-    res.status(400).json({ error: errors.join(",") })
+    res.status(400).json({ error: errors.join(',') })
     return
   }
   mintProposalModel
     .updateProposal(req.body)
     .then(() =>
       res.json({
-        message: "success"
+        message: 'success'
       })
     )
     .catch((err) => res.status(400).json({ error: err.message }))
 })
 
 
-app.post("/api/proposal/find", (req, res, next) => {
+app.post('/api/proposal/find', (req, res, next) => {
   var errors = []
-  if (req.body.proposalId === undefined) errors.push("No proposal id specified")
+  if (req.body.proposalId === undefined) errors.push('No proposal id specified')
   if (errors.length) {
-    res.status(400).json({ error: errors.join(",") })
+    res.status(400).json({ error: errors.join(',') })
     return
   }
   mintProposalModel
     .findProposal(req.body)
     .then((proposal) =>
       res.json({
-        message: "success",
+        message: 'success',
         data: proposal
       })
     )
     .catch((err) => res.status(400).json({ error: err.message }))
 })
 
-app.post("/api/proposal/", (req, res, next) => {
+app.post('/api/proposal/', (req, res, next) => {
   var errors = []
   const { offset, limit } = req.body
-  if (offset === undefined) errors.push("No offset specified")
-  if (limit === undefined) errors.push("No limit specified")
+  if (offset === undefined) errors.push('No offset specified')
+  if (limit === undefined) errors.push('No limit specified')
   if (errors.length) {
-    res.status(400).json({ error: errors.join(",") })
+    res.status(400).json({ error: errors.join(',') })
     return
   }
   mintProposalModel
     .getMintProposals(offset, limit)
     .then((proposals) =>
       res.json({
-        message: "success",
+        message: 'success',
         data: proposals
       })
     )
     .catch((err) => res.status(400).json({ error: err.message }))
 })
 
-app.post("/api/proposal/mint/create", (req, res, next) => {
+app.post('/api/proposal/mint/create', (req, res, next) => {
   var errors = []
   //const data = JSON.parse(Object.keys(req.body)[0])
   const {
@@ -123,16 +124,16 @@ app.post("/api/proposal/mint/create", (req, res, next) => {
     transaction_hash,
     propose_time
   } = req.body
-  if (!proposal_id) errors.push("No proposal id specified")
-  if (!proposer) errors.push("No proposer specified")
-  if (!receiver) errors.push("No receiver specified")
-  if (!amount) errors.push("No amount specified")
-  if (!description) errors.push("No description specified")
-  if (!propose_time) errors.push("No propose time specified")
-  if (!transaction_hash) errors.push("No transaction hash specified")
+  if (!proposal_id) errors.push('No proposal id specified')
+  if (!proposer) errors.push('No proposer specified')
+  if (!receiver) errors.push('No receiver specified')
+  if (!amount) errors.push('No amount specified')
+  if (!description) errors.push('No description specified')
+  if (!propose_time) errors.push('No propose time specified')
+  if (!transaction_hash) errors.push('No transaction hash specified')
 
   if (errors.length) {
-    res.status(400).json({ error: errors.join(",") })
+    res.status(400).json({ error: errors.join(',') })
     return
   }
 
@@ -155,7 +156,7 @@ app.post("/api/proposal/mint/create", (req, res, next) => {
             .addHolders(newHolders.addresses, newHolders.names)
             .then(() => {
               res.json({
-                message: "success"
+                message: 'success'
               })
             })
             .catch((err) => res.status(400).json({ error: err.message }))
@@ -165,7 +166,7 @@ app.post("/api/proposal/mint/create", (req, res, next) => {
     .catch((err) => res.status(400).json({ error: err.message }))
 })
 
-app.post("/api/proposal/batchmint/create", (req, res, next) => {
+app.post('/api/proposal/batchmint/create', (req, res, next) => {
   var errors = []
   //const data = JSON.parse(Object.keys(req.body)[0])
   const {
@@ -177,16 +178,16 @@ app.post("/api/proposal/batchmint/create", (req, res, next) => {
     proposeTx,
     proposeTime
   } = req.body
-  if (!proposalId) errors.push("No proposal id specified")
-  if (!proposer) errors.push("No proposer specified")
-  if (!receivers) errors.push("No receivers specified")
-  if (!amounts) errors.push("No amounts specified")
-  if (!description) errors.push("No description specified")
-  if (!proposeTime) errors.push("No propose time specified")
-  if (!proposeTx) errors.push("No transaction hash specified")
+  if (!proposalId) errors.push('No proposal id specified')
+  if (!proposer) errors.push('No proposer specified')
+  if (!receivers) errors.push('No receivers specified')
+  if (!amounts) errors.push('No amounts specified')
+  if (!description) errors.push('No description specified')
+  if (!proposeTime) errors.push('No propose time specified')
+  if (!proposeTx) errors.push('No transaction hash specified')
 
   if (errors.length) {
-    res.status(400).json({ error: errors.join(",") })
+    res.status(400).json({ error: errors.join(',') })
     return
   }
 
@@ -208,7 +209,7 @@ app.post("/api/proposal/batchmint/create", (req, res, next) => {
             .addHolders(newHolders.addresses, newHolders.names)
             .then(() => {
               res.json({
-                message: "success"
+                message: 'success'
               })
             })
             .catch((err) => res.status(400).json({ error: err.message }))
