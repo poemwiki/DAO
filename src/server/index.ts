db10f4e25abeda21599c6f6ce6dbdd619408c6db
Ray Victor <ray7551@gmail.com>
2024-04-08 18:47:28 +0800

    move static files to /public

--- src/server/index.ts
+++ src/server/index.ts
@@ -17,7 +17,7 @@ const port = process.env.PORT || 3000
 app.set('view engine', 'ejs')
 app.set('views', 'src/server/views')
 
-app.use(express.static(path.join(__dirname, '../statics')))
+app.use(express.static(path.join(__dirname, '../../public')))
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
